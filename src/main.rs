9828bcf81fd88b41d0868b0208daf47d7fe74a88
Nick Fitzgerald <fitzgen@gmail.com>
2018-12-26 15:46:32 +0900

    Use fart

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use fart::svg::{self, node::element};
+
+fn gen(config: &mut fart::Config, document: svg::Document) -> fart::Result<()> {
+    let data = element::Data::new()
+        .move_to((10, 10))
+        .line_by((0, 50))
+        .line_by((50, 0))
+        .line_by((0, -50))
+        .close();
+
+    let path = element::Path::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 3)
+        .set("d", data);
+
+    document.set("viewBox", (0, 0, 70, 70)).add(path)
+}
+
 fn main() {
-    println!("Hello, world!");
+    fart::generate(gen);
 }
