c9dc869cf3ec24f6faf2bfd28890ecba4ba43de7
Nick Fitzgerald <fitzgen@gmail.com>
2019-03-24 11:20:23 -0700

    Update to fart 0.3.0

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,19 @@
-use fart::{aabb, path, scene, shape, svg, Point2};
+use fart::prelude::*;
+use fart::shape;
 
 fn main() {
-    fart::generate(gen);
-}
+    fart::generate(|cfg| {
+        let mut scene = Scene::new(Aabb::new(point2(0, 0), point2(1000, 1000)));
 
-fn gen(_cfg: &mut fart::Config) -> fart::Result<svg::Document> {
-    let mut scene = scene::Scene::new(aabb::AxisAlignedBoundingBox::new(
-        Point2::new(0.0, 0.0),
-        Point2::new(1000.0, 1000.0),
-    ));
+        let x_dist = Uniform::new(0, 1000);
+        let y_dist = Uniform::new(0, 1000);
 
-    scene.add(Triangle(
-        Point2::new(500.0, 250.0),
-        Point2::new(750.0, 750.0),
-        Point2::new(250.0, 750.0),
-    ));
+        scene.add(shape::Triangle {
+            a: point2(x_dist.sample(cfg.rng()), y_dist.sample(cfg.rng())),
+            b: point2(x_dist.sample(cfg.rng()), y_dist.sample(cfg.rng())),
+            c: point2(x_dist.sample(cfg.rng()), y_dist.sample(cfg.rng())),
+        });
 
-    Ok(scene.create_svg(scene::Inches(7.0), scene::Inches(7.0)))
-}
-
-#[derive(Debug)]
-struct Triangle(Point2, Point2, Point2);
-
-impl shape::Shape for Triangle {
-    fn paths(&self) -> Vec<path::Path> {
-        vec![path::Path::with_commands(vec![
-            path::LineCommand::MoveTo(self.0),
-            path::LineCommand::LineTo(self.1),
-            path::LineCommand::LineTo(self.2),
-            path::LineCommand::Close,
-        ])]
-    }
-
-    fn aabb(&self) -> aabb::AxisAlignedBoundingBox {
-        use std::f64;
-        let mut min = self.0;
-        let mut max = self.0;
-        for p in [self.1, self.2].iter() {
-            min.x = f64::min(min.x, p.x);
-            min.y = f64::min(min.y, p.y);
-            max.x = f64::max(max.x, p.x);
-            max.y = f64::max(max.y, p.y);
-        }
-        aabb::AxisAlignedBoundingBox::new(min, max)
-    }
+        Ok(scene.create_svg(Inches(7.0), Inches(7.0)))
+    });
 }
